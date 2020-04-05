f5b1d8c1215ec063986c45174184bfe42d5bc216
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-05 22:05:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,7 @@ def monopoly_with_third_tier
       },
       :names => {
         :reading_railroad => {},
-        :pennslyvania_railroad => {},
+        :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         
       }
